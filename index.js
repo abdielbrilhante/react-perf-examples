e95ca8244661b6a6624e8e7e185a94e72c5ca0fe
Abdiel Brilhante <abdiel.brilhante@gmail.com>
2020-01-12 13:09:28 -0300

    Add base app and parcel config

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
